41882ebbea0d501b24a426b3edee1988cb00ca2e
Bartek <baqbaek@gmail.com>
2024-04-21 19:10:17 +0200

    ikonka

--- app/index.php
+++ app/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteka Online</title>
+    <link rel="icon" href="images/icon.png" type="image/png">
     <!-- Dodanie stylów Bootstrap -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
@@ -39,6 +40,7 @@
         </div>
     </footer>
     <!-- Dodanie skryptów Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
